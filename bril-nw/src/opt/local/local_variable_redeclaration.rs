4aaf766f0ea7368f8cdd4beacc6cc53af1cfcf59
Nick Wong <tginick93@gmail.com>
2022-06-23 01:30:48 -0700

    impl local variable redecl

--- bril-nw/src/opt/local/local_variable_redeclaration.rs
+++ bril-nw/src/opt/local/local_variable_redeclaration.rs
@@ -1,6 +1,10 @@
-use std::collections::HashSet;
+use std::{
+    collections::{HashMap, HashSet},
+    mem,
+    rc::Rc,
+};
 
-use crate::{basicblock::BasicBlock, opt::LocalOptimizationPass};
+use crate::{basicblock::BasicBlock, bril::types::Instruction, opt::LocalOptimizationPass};
 
 pub struct LocalVariableRedeclaration();
 
@@ -27,10 +31,9 @@ pub struct LocalVariableRedeclaration();
 */
 impl LocalOptimizationPass for LocalVariableRedeclaration {
     fn run(block: &mut BasicBlock) {
-        // can be written more optimally, but running to convergence WILL work
         loop {
-            let should_continue = delete_unused_assignments(block);
-            if !should_continue {
+            let did_delete_instructions = delete_unused_assignments(block);
+            if !did_delete_instructions {
                 break;
             }
         }
@@ -38,10 +41,41 @@ impl LocalOptimizationPass for LocalVariableRedeclaration {
 }
 
 fn delete_unused_assignments(block: &mut BasicBlock) -> bool {
-    let mut already_assigned = HashSet::<String>::new();
-    let mut reassignments_removed = 0;
+    let mut last_def: HashMap<String, Rc<Instruction>> = HashMap::new();
+    let mut instrs_to_delete: HashSet<*const Instruction> = HashSet::new();
+    for instr in &block.instrs {
+        // check for uses
+        let maybe_args = instr.get_args();
+        if let Some(args) = maybe_args {
+            for arg in args {
+                last_def.remove(arg);
+            }
+        }
 
-    for instr in &block.instrs {}
+        // check for assigns
+        let maybe_dest = instr.get_dest();
+        if let Some(dest) = maybe_dest {
+            if last_def.contains_key(dest) {
+                // actually stage the instruction for deletion
+                instrs_to_delete.insert(Rc::as_ptr(last_def.get(dest).unwrap()));
+            }
+            last_def.insert(dest.to_string(), instr.clone());
+        }
+    }
 
-    reassignments_removed > 0
+    if instrs_to_delete.is_empty() {
+        return false;
+    }
+
+    let mut new_instrs: Vec<Rc<Instruction>> = Vec::new();
+    mem::swap(&mut new_instrs, &mut block.instrs);
+
+    let mut filtered_instrs = new_instrs
+        .into_iter()
+        .filter(|instr| !instrs_to_delete.contains(&Rc::as_ptr(instr)))
+        .collect();
+
+    mem::swap(&mut filtered_instrs, &mut block.instrs);
+
+    return true;
 }
